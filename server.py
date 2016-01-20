4491436721e209301ff375c98d4df8c1cbb05a7f
yoavrab <yoavr1106@gmail.com>
2016-01-20 16:29:49 +0200

    adding threading
    adding threading and another user in order to check it

--- server.py
+++ server.py
@@ -1,10 +1,25 @@
 import socket
+import threading
+
+def handle_client():
+    # accept a client and handling it
+    (client_socket, client_address) = my_socket.accept()
+    data = client_socket.recv(1024)
+    client_socket.send('hello2')
+    print data
+    client_socket.close()
 
 my_socket = socket.socket()
 my_socket.bind(('0.0.0.0',8820))
-my_socket.listen(1)
-(client_socket, client_address) = my_socket.accept()
-data= client_socket.recv(1024)
-client_socket.send('hello2')
-print data
-client_socket.close()
+
+for j in range(3):
+    my_socket.listen(1)
+
+
+threads = []
+for i in range(5):
+    t = threading.Thread(target=handle_client())
+    threads.append(t)
+    t.start()
+
+
